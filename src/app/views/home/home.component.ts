ff0ea0e79605750a244cb180e5043fe43c8a9fdc
andregomars <andregomars@gmail.com>
2020-04-11 14:28:08 -0700

    new version

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = 'Regular Ng8';
+  title = 'Regular Ng8 new';
   show$ = new BehaviorSubject<boolean>(false);
 
   constructor() { }
